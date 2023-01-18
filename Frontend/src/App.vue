2271cf7449edc22f749a46e265d2bf9dfdde5377
Adnan Kamali <adnankamali1246@gmail.com>
2023-01-18 22:58:36 +0330

    add navbar to page

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <AppBar />
+  <v-app theme="dark">
+    <AppBar />
+
+  </v-app>
 </template>
 
 <script>
@@ -10,3 +13,10 @@ export default {
   },
 };
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Vazirmatn:wght@500&display=swap');
+*{
+  font-family: Vazirmatn;
+}
+</style>
--- Frontend/src/components/AppBar.vue
+++ Frontend/src/components/AppBar.vue
@@ -1,3 +1,45 @@
 <template>
+  <v-app-bar elevation="0" color="black" class="d-flex justify-end">
 
+    <v-app-bar-title class="d-flex justify-center">
+      I'm Adnan
+    </v-app-bar-title>
+    <v-app-bar-nav-icon icon="menu" @click.stop="drawer = !drawer"/>
+  </v-app-bar>
+  <v-navigation-drawer
+      location="right"
+      color="black"
+    v-model="drawer"
+  >
+    <v-list>
+      <v-list-item
+          prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+          title="Adnan Kamali"
+          subtitle="adnankamali1246@gmail.com"
+      ></v-list-item>
+    <v-divider/>
+      <v-list-item
+        v-for="item in items"
+        :key="item.icon"
+        :title="item.title"
+        :append-icon="item.icon"
+        :value="item.value"
+      />
+    </v-list>
+  </v-navigation-drawer>
 </template>
+
+<script>
+export default{
+  data(){
+    return {
+      drawer: false,
+      items:[
+        {title: "من کی هستم", value:"Dsh", icon: "person"},
+        {title: "مهارت ها", value:"St", icon:"category"},
+        {title: "ارتباط با من", value:"As", icon:"contacts"},
+      ],
+    }
+  }
+}
+</script>
