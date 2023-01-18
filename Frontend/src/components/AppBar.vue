c32d58f968c667a6968ec830e0344c751b408eb3
Adnan Kamali <adnankamali1246@gmail.com>
2023-01-18 23:10:11 +0330

    add avatar to appbar

--- Frontend/src/components/AppBar.vue
+++ Frontend/src/components/AppBar.vue
@@ -1,23 +1,19 @@
 <template>
   <v-app-bar elevation="0" color="black" class="d-flex justify-end">
-
-    <v-app-bar-title class="d-flex justify-center">
-      I'm Adnan
-    </v-app-bar-title>
-    <v-app-bar-nav-icon icon="menu" @click.stop="drawer = !drawer"/>
+    <v-avatar class="ml-3">
+      <v-img src="https://randomuser.me/api/portraits/women/85.jpg"> </v-img>
+    </v-avatar>
+    <v-app-bar-title class="d-flex justify-start"> I'm Adnan </v-app-bar-title>
+    <v-app-bar-nav-icon icon="menu" @click.stop="drawer = !drawer" />
   </v-app-bar>
-  <v-navigation-drawer
-      location="right"
-      color="black"
-    v-model="drawer"
-  >
+  <v-navigation-drawer location="right" color="black" v-model="drawer">
     <v-list>
       <v-list-item
-          prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
-          title="Adnan Kamali"
-          subtitle="adnankamali1246@gmail.com"
+        prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+        title="Adnan Kamali"
+        subtitle="adnankamali1246@gmail.com"
       ></v-list-item>
-    <v-divider/>
+      <v-divider />
       <v-list-item
         v-for="item in items"
         :key="item.icon"
@@ -30,16 +26,16 @@
 </template>
 
 <script>
-export default{
-  data(){
+export default {
+  data() {
     return {
       drawer: false,
-      items:[
-        {title: "من کی هستم", value:"Dsh", icon: "person"},
-        {title: "مهارت ها", value:"St", icon:"category"},
-        {title: "ارتباط با من", value:"As", icon:"contacts"},
+      items: [
+        { title: "من کی هستم", value: "Dsh", icon: "person" },
+        { title: "مهارت ها", value: "St", icon: "category" },
+        { title: "ارتباط با من", value: "As", icon: "contacts" },
       ],
-    }
-  }
-}
+    };
+  },
+};
 </script>
